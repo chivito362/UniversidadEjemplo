b8fd5f0ddef25039517c21b4d263046ef07cf448
chivito362 <115670711+chivito362@users.noreply.github.com>
2023-09-08 16:48:49 -0300

    Entidades

--- src/universidadejemplo/Entidades/Alumno.java
+++ src/universidadejemplo/Entidades/Alumno.java
@@ -5,10 +5,76 @@
  */
 package universidadejemplo.Entidades;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Sebastian
  */
 public class Alumno {
+    private int idAlumno;
+    private String apellido;
+    private String nombre;
+    private LocalDate gechaNac;
+    private boolean estado;
+
+    public Alumno() {
+    }
+
+    public Alumno(int idAlumno, String apellido, String nombre, LocalDate gechaNac, boolean estado) {
+        this.idAlumno = idAlumno;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.gechaNac = gechaNac;
+        this.estado = estado;
+    }
+
+    public Alumno(String apellido, String nombre, LocalDate gechaNac, boolean estado) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.gechaNac = gechaNac;
+        this.estado = estado;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getGechaNac() {
+        return gechaNac;
+    }
+
+    public void setGechaNac(LocalDate gechaNac) {
+        this.gechaNac = gechaNac;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
     
 }
--- src/universidadejemplo/Entidades/Materia.java
+++ src/universidadejemplo/Entidades/Materia.java
@@ -10,5 +10,58 @@ package universidadejemplo.Entidades;
  * @author Sebastian
  */
 public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int anioMateria;
+    private boolean estado;
+
+    public Materia() {
+    }
+
+    public Materia(int idMateria, String nombre, int anioMateria, boolean estado) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.estado = estado;
+    }
+
+    public Materia(String nombre, int anioMateria, boolean estado) {
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.estado = estado;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioMateria() {
+        return anioMateria;
+    }
+
+    public void setAnioMateria(int anioMateria) {
+        this.anioMateria = anioMateria;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
     
 }
