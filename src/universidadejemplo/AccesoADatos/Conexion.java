a9332bd181cd11dc36291dd6e2bfdec5b92cd80c
LJohaGuevara <joha.guevara15@gmail.com>
2023-09-08 17:50:15 -0300

    Conexion

--- src/universidadejemplo/AccesoADatos/Conexion.java
+++ src/universidadejemplo/AccesoADatos/Conexion.java
@@ -5,10 +5,53 @@
  */
 package universidadejemplo.AccesoADatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Sebastian
  */
 public class Conexion {
-    
+    private  static final String url = "jdbc:mariadb://localhost:3306/";
+    private static final String db = "universidadulp";
+    private static final String usuario = "root";
+    private  static String contrasenia= "";
+    private static Connection conexion;
+
+    public Conexion() {
+    } 
+
+        
+        public static Connection getConection () {
+            
+            if (conexion == null) {
+                
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    
+                    //establecer la conexion
+                    
+                    conexion = DriverManager.getConnection(url+db, usuario, contrasenia);
+                            
+                            } catch (ClassNotFoundException ex) {
+                    JOptionPane.showMessageDialog(null, "Error de driver");
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "Error de conexion");
+                }
+                
+            }
+        return conexion;
+            
+    }
 }
+    
+
+    
+    
+    
+
